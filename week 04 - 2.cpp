8be6937d38971e9d869e99ad9ee2cd3e98316985
Brad100707 <57812845+Brad100707@users.noreply.github.com>
2021-10-07 16:49:10 +0800

    Add files via upload

---
+++ week 04 - 2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+
+    float  a = 3.14159265358979;
+    double b = 3.14159265358979;
+    printf("%f\n", a);
+    printf("%f\n", b);
+    printf("%.4f\n", b);
+    printf("%.14f\n", a);
+    printf("%.13f\n", b);
+
+
+return 0;
+
+
+}
---
+++ week 04 -1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+
+ int a = 133, b = 25;
+
+ printf("%d\n", a + b);
+ printf("%5d\n", a + b);
+ return 0;
+
+
+
+
+
+}
