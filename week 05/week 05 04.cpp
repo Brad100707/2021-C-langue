d5d8b00d451552ce18af1d31acc3613a4b1173b4
Brad100707 <57812845+Brad100707@users.noreply.github.com>
2021-10-14 17:43:53 +0800

    Add files via upload

---
+++ week 05/week 05 04.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+
+
+if(1) {printf("成立\n");}
+if(2) {printf("成立\n");}
+if(3) {printf("成立\n");}
+if(-1) {printf("成立\n");}
+if(-2) {printf("成立\n");}
+if(0) {printf("不成立\n");}
+return 0;
+
+
+
+
+
+
+}
